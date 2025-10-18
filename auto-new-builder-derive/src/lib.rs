bf3eb671a92c7c50f96cc586306fc186ab9e85ae
K4 <kaishusahu101@gmail.com>
2025-10-18 13:58:26 +0530

    add support for new type

--- auto-new-builder-derive/src/lib.rs
+++ auto-new-builder-derive/src/lib.rs
@@ -1,5 +1,5 @@
 use quote::quote;
-use syn::{DeriveInput, Type};
+use syn::{DataStruct, DeriveInput, Ident, Type};
 use utils::get_struct_name;
 
 mod utils;
@@ -11,60 +11,53 @@ pub fn auto_new_builder_derive(input: proc_macro::TokenStream) -> proc_macro::To
     output_stream.into()
 }
 
-fn generate(input_stream: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
-    let DeriveInput { data, .. } = syn::parse2(input_stream.clone()).unwrap();
-    let struct_name = get_struct_name(input_stream.clone());
-
+fn generate_struct(struct_name: Ident, data_struct: DataStruct) -> proc_macro2::TokenStream {
     let mut new_stream = Vec::<proc_macro2::TokenStream>::new();
     let mut field_stream = Vec::<proc_macro2::TokenStream>::new();
     let mut with_fn_stream = Vec::<proc_macro2::TokenStream>::new();
 
-    if let syn::Data::Struct(data_struct) = data {
-        let mut has_optional_fields_started = false;
+    let mut has_optional_fields_started = false;
 
-        for field in data_struct.fields {
-            let field_name = field.clone().ident.unwrap();
-            let field_attr = field.clone().attrs;
+    for field in data_struct.fields {
+        let field_name = field.clone().ident.unwrap();
+        let field_attr = field.clone().attrs;
 
-            let mut auto_new_value = None;
-            let mut auto_new_required = false;
+        let mut auto_new_value = None;
+        let mut auto_new_required = false;
 
-            for attr in field_attr {
-                if let syn::Meta::NameValue(meta) = &attr.meta {
-                    if meta.path.is_ident("auto_new_value") {
-                        match &meta.value {
-                            syn::Expr::Lit(expr_lit) => match &expr_lit.lit {
-                                syn::Lit::Str(lit_str) => {
-                                    let value_str = lit_str.value();
-                                    let tokens = value_str
-                                        .parse::<proc_macro2::TokenStream>()
-                                        .expect("Failed to parse string as token stream");
-                                    auto_new_value = Some(tokens);
-                                }
-                                lit => {
-                                    auto_new_value = Some(quote::quote!(#lit));
-                                }
-                            },
-                            _ => panic!("auto_new_value must be a literal"),
-                        }
-                        break;
+        for attr in field_attr {
+            if let syn::Meta::NameValue(meta) = &attr.meta {
+                if meta.path.is_ident("auto_new_value") {
+                    match &meta.value {
+                        syn::Expr::Lit(expr_lit) => match &expr_lit.lit {
+                            syn::Lit::Str(lit_str) => {
+                                let value_str = lit_str.value();
+                                let tokens = value_str
+                                    .parse::<proc_macro2::TokenStream>()
+                                    .expect("Failed to parse string as token stream");
+                                auto_new_value = Some(tokens);
+                            }
+                            lit => {
+                                auto_new_value = Some(quote::quote!(#lit));
+                            }
+                        },
+                        _ => panic!("auto_new_value must be a literal"),
                     }
-                } else if let syn::Meta::Path(meta) = &attr.meta {
-                    if meta.is_ident("auto_new_required") {
-                        auto_new_required = true;
-                        break;
-                    }
+                    break;
+                }
+            } else if let syn::Meta::Path(meta) = &attr.meta {
+                if meta.is_ident("auto_new_required") {
+                    auto_new_required = true;
+                    break;
                 }
             }
+        }
 
-            match field.clone().ty {
-                Type::Path(type_path) => {
-                    if type_path.path.segments.len() == 1
-                        && type_path.path.segments[0].ident == "Option"
-                    {
-                        if let syn::PathArguments::AngleBracketed(args) =
-                            &type_path.path.segments[0].arguments
-                        {
+        match field.clone().ty {
+            Type::Path(type_path) => {
+                if let Some(last_segment) = type_path.path.segments.last() {
+                    if last_segment.ident == "Option" {
+                        if let syn::PathArguments::AngleBracketed(args) = &last_segment.arguments {
                             if args.args.len() == 1 {
                                 has_optional_fields_started = true;
                                 let inner_type = &args.args[0];
@@ -108,58 +101,198 @@ fn generate(input_stream: proc_macro2::TokenStream) -> proc_macro2::TokenStream 
                         }
                     }
                 }
-                _ => {
-                    panic!("Unsupported type in generic");
-                }
-            };
-
-            if has_optional_fields_started {
-                panic!("Optional Fields should be the at the last")
             }
-
-            let field_type = match field.ty {
-                Type::Path(type_path) => type_path.path,
-                _ => {
-                    panic!("Unsupported type in generic");
-                }
-            };
-
-            if auto_new_value.is_some() {
-                field_stream.push(quote! {
-                    #field_name : #auto_new_value
-                });
-                let with_method_name =
-                    syn::Ident::new(&format!("with_{}", field_name), field_name.span());
-
-                with_fn_stream.push(quote! {
-                    #[inline]
-                    pub fn #with_method_name(mut self, value: #field_type) -> Self {
-                        self.#field_name = value;
-                        self
-                    }
-                });
-                continue;
+            _ => {
+                panic!("Unsupported type in generic");
             }
+        };
 
-            new_stream.push(quote! {
-                #field_name: #field_type
-            });
-
-            field_stream.push(quote! {
-                #field_name : #field_name
-            });
+        if has_optional_fields_started {
+            panic!("Optional Fields should be the at the last")
         }
 
-        quote! {
-            impl #struct_name {
-                pub fn new(#(#new_stream),*) -> Self {
-                    #struct_name {
-                        #(#field_stream),*
+        let field_type = match field.ty {
+            Type::Path(type_path) => type_path.path,
+            _ => {
+                panic!("Unsupported type in generic");
+            }
+        };
+
+        if auto_new_value.is_some() {
+            field_stream.push(quote! {
+                #field_name : #auto_new_value
+            });
+            let with_method_name =
+                syn::Ident::new(&format!("with_{}", field_name), field_name.span());
+
+            with_fn_stream.push(quote! {
+                #[inline]
+                pub fn #with_method_name(mut self, value: #field_type) -> Self {
+                    self.#field_name = value;
+                    self
+                }
+            });
+            continue;
+        }
+
+        new_stream.push(quote! {
+            #field_name: #field_type
+        });
+
+        field_stream.push(quote! {
+            #field_name : #field_name
+        });
+    }
+
+    quote! {
+        impl #struct_name {
+            pub fn new(#(#new_stream),*) -> Self {
+                #struct_name {
+                    #(#field_stream),*
+                }
+            }
+
+            #(#with_fn_stream)*
+
+        }
+    }
+}
+
+fn generate_newtype(struct_name: Ident, data_struct: DataStruct) -> proc_macro2::TokenStream {
+    let mut new_stream = Vec::<proc_macro2::TokenStream>::new();
+    let mut field_stream = Vec::<proc_macro2::TokenStream>::new();
+    let mut with_fn_stream = Vec::<proc_macro2::TokenStream>::new();
+
+    let mut auto_new_value = None;
+    let mut auto_new_required = false;
+
+    if let syn::Fields::Unnamed(fields) = &data_struct.fields {
+        let field = fields.unnamed[0].clone();
+        let field_attr = field.clone().attrs;
+
+        for attr in field_attr {
+            if let syn::Meta::NameValue(meta) = &attr.meta {
+                if meta.path.is_ident("auto_new_value") {
+                    match &meta.value {
+                        syn::Expr::Lit(expr_lit) => match &expr_lit.lit {
+                            syn::Lit::Str(lit_str) => {
+                                let value_str = lit_str.value();
+                                let tokens = value_str
+                                    .parse::<proc_macro2::TokenStream>()
+                                    .expect("Failed to parse string as token stream");
+                                auto_new_value = Some(tokens);
+                            }
+                            lit => {
+                                auto_new_value = Some(quote::quote!(#lit));
+                            }
+                        },
+                        _ => panic!("auto_new_value must be a literal"),
+                    }
+                    break;
+                }
+            } else if let syn::Meta::Path(meta) = &attr.meta {
+                if meta.is_ident("auto_new_required") {
+                    auto_new_required = true;
+                    break;
+                }
+            }
+        }
+
+        match field.clone().ty {
+            Type::Path(type_path) => {
+                if let Some(last_segment) = type_path.path.segments.last() {
+                    if last_segment.ident == "Option" {
+                        if let syn::PathArguments::AngleBracketed(args) = &last_segment.arguments {
+                            if args.args.len() == 1 {
+                                let inner_type = &args.args[0];
+
+                                if auto_new_value.is_some() {
+                                    field_stream.push(quote! {
+                                        #auto_new_value
+                                    });
+                                } else if auto_new_required {
+                                    new_stream.push(quote! {
+                                        inner: #inner_type
+                                    });
+                                    field_stream.push(quote! {
+                                        Some(inner)
+                                    });
+                                } else {
+                                    field_stream.push(quote! {
+                                        None
+                                    });
+                                }
+
+                                with_fn_stream.push(quote! {
+                                    #[inline]
+                                    pub fn with_inner(mut self, value: #inner_type) -> Self {
+                                        self.0 = Some(value);
+                                        self
+                                    }
+                                });
+                            } else {
+                                panic!("Option must have exactly one type parameter");
+                            }
+                        } else {
+                            panic!("Invalid Option type format");
+                        }
+                    } else {
+                        let field_type = type_path.path;
+
+                        if auto_new_value.is_some() {
+                            field_stream.push(quote! {
+                                #auto_new_value
+                            });
+                            with_fn_stream.push(quote! {
+                                #[inline]
+                                pub fn with_inner(mut self, value: #field_type) -> Self {
+                                    self.0 = value;
+                                    self
+                                }
+                            });
+                        } else {
+                            new_stream.push(quote! {
+                                inner: #field_type
+                            });
+                            field_stream.push(quote! {
+                                inner
+                            });
+                        }
                     }
                 }
+            }
+            _ => {
+                panic!("Unsupported type in generic");
+            }
+        };
 
+        return quote! {
+            impl #struct_name {
+                pub fn new(#(#new_stream),*) -> Self {
+                    #struct_name (#(#field_stream),*)
+                }
                 #(#with_fn_stream)*
+            }
+        };
+    }
+    panic!("Unsupported type");
+}
 
+fn generate(input_stream: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
+    let DeriveInput { data, .. } = syn::parse2(input_stream.clone()).unwrap();
+    let struct_name = get_struct_name(input_stream.clone());
+
+    if let syn::Data::Struct(data_struct) = data {
+        match &data_struct.fields {
+            syn::Fields::Named(_) => generate_struct(struct_name, data_struct),
+            syn::Fields::Unnamed(fields) if fields.unnamed.len() == 1 => {
+                generate_newtype(struct_name, data_struct)
+            }
+            syn::Fields::Unnamed(_) => {
+                panic!("Tuple structs with multiple fields are not supported");
+            }
+            syn::Fields::Unit => {
+                panic!("Unit structs are not supported");
             }
         }
     } else {
--- example/src/main.rs
+++ example/src/main.rs
@@ -12,6 +12,10 @@ fn main() {
     println!("{:?}", lello.world);
     println!("{:?}", lello.next_world);
 
+    let _hello2 = Hello2::new(42);
+    let _hello3 = Hello3::new();
+    let _hello4 = Hello4::new(42);
+
 }
 
 #[derive(AutoNewBuilder, Debug)]
@@ -21,16 +25,37 @@ pub struct Hello {
     universe: u32,
     #[auto_new_value="\"HELLO\".to_string()"]
     next_next_world: String, 
-    next_world: Option<u32>,
+    next_world: ::std::option::Option<u32>,
     #[auto_new_required]
     next_universe: Option<u32>,
     #[auto_new_value="say()"]
     super_next_next_world: Option<u32>, 
     #[auto_new_value="Some(46)"]
     super_super_next_next_world: Option<u32>, 
-    
 }
 
 pub fn say() -> Option<u32> {
     Some(42)
 }
+
+#[derive(AutoNewBuilder, Debug)]
+pub struct Hello2 (u32);
+
+
+#[derive(AutoNewBuilder, Debug)]
+pub struct Hello3 (#[auto_new_value="42"] u32);
+
+#[derive(AutoNewBuilder, Debug)]
+pub struct Hello4 (
+    #[auto_new_required]
+    Option<u32>
+);
+
+// #[derive(AutoNewBuilder, Debug)]
+// pub struct Hello4 (u32, u32);
+
+// #[derive(AutoNewBuilder, Debug)]
+// pub struct Hello5 (u32, Option<u32>);
+
+// #[derive(AutoNewBuilder, Debug)]
+// pub struct Hello6 (::std::option::Option<u32>);
